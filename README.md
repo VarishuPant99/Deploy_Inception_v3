dd0fa0ebd1590412aaa3a60bd13f8197b3279aaf
aman5319 <amanpandey5319@gmail.com>
2020-04-03 22:42:42 +0530

    Readme update and Inference file update

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# flask_image_classification
+# Flask Image Classification
+
+This repo comprises of training DenseNet from scratch on cifar10 and then using Flask for Model serving.
+
+The directory is defined as 
+
+![](Screenshot from 2020-04-03 22-17-17.png)
+
+## For Training
+
+[Run Training Notebook file](TrainingNotebook.ipynb)
+
+
+
+## For Inference
+
+ we need Cifar-10 data for inference.
+
+Execute the below to generate 10 random images from cifar-10 test data.
+
+```bash
+cd data
+python generate_valid_data.py
+```
+
+
+
+For Serving this model as a website. First generate  test images by executing above commands and then.
+
+
+```bash
+python flask_api/app.py
+```
--- src/inference.py
+++ src/inference.py
@@ -1,13 +1,14 @@
 from .model import getModel
-from .data import Cifar10Data
 from pathlib import Path
 import PIL
 import torch
+from torchvision import  transforms
+
 
 class Inference:
     def __init__(self, save_model_filename="saved_weights.pt"):
-        self.data = Cifar10Data()
-        self.model = getModel(training=False,num_classes=self.data.num_classes)
+        self.classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship','truck']
+        self.model = getModel(training=False,num_classes=len(self.classes))
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.load_state_dict(torch.load(f"./src/saved_weights/{save_model_filename}",map_location=device))
         return None
@@ -17,9 +18,9 @@ class Inference:
             image = PIL.Image.open(image).convert("RGB")
         elif not isinstance(image,PIL.JpegImagePlugin.JpegImageFile): 
             raise Exception("must be PIL image or path ")
-        image_input = self.data.val_transforms(image).unsqueeze(0)
+        image_input = transforms.ToTensor()(image).unsqueeze(0)
         with torch.no_grad():
             out = self.model(image_input).squeeze(0)
             prob = torch.argmax(out).item()
-            return self.data.trainset.classes[prob]
+            return self.classes[prob]
 
